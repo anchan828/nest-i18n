fbe954e5e5ab849d3890afabfbd47ecc5ad8e25c
anchan828 <anchan828@gmail.com>
2019-09-19 17:21:37 +0900

    fix: add exports

--- packages/i18next/src/index.ts
+++ packages/i18next/src/index.ts
@@ -1,2 +1,5 @@
+export { I18NEXT_INIT_OPTIONS } from "./constants";
 export * from "./exception.filter";
+export * from "./exceptions";
 export * from "./i18next.module";
+export * from "./interfaces";
